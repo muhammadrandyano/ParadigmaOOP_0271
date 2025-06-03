e0b16de58eb02ce1926b8da764807b49fb42106c
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-06-03 11:08:32 +0700

    Menambahkan int main pada class remoteLampu

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -11,3 +11,19 @@ class remoteLampu {
         return saklarNo [i];
     }
 };
+
+int main(){
+    remoteLampu LampuRumah;
+
+    LampuRumah.setSaklarNo(0, "Lampu Teras RumahQ")
+    LampuRumah.setSaklarNO(1, "Lampu Ruang Tamu")
+    LampuRumah.setSaklarNo(2, "Lampu Kamar Tidur")
+    LampuRumah.setSaklarNo(3, "Lampu Dapur")
+
+    cout << LampuRumah.getSaklarNo(0) << endl;
+    cout << LampuRumah.getSaklarNo(1) << endl;
+    cout << LampuRumah.getSaklarNo(2) << endl;
+    cout << LampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
+}
