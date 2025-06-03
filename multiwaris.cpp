c854dccda4bac6f66926c7e66fb6c4d0337ecdd9
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-06-03 10:41:23 +0700

    Membuat class orang

---
+++ multiwaris.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class orang{
+    public :
+    int Umur;
+
+    orang (int pUmur):
+    Umur (pUmur) 
+    {
+        cout << "orang dibuat dengan umur" << Umur <<"\n" << endl;
+    } 
+};
