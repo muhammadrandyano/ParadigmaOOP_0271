b119c84fde82140ab19932edad1cd857e3b0e0ec
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-06-03 11:06:01 +0700

    Membuatkan class abstraksi dengan menambahkan nilai

---
+++ abstraksi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Abstraksiklas {
+    private : string x,y;
+
+    public :
+
+    //method untuk mengisi nilai
+    //private member
+    
+    void setXY(string a, string b) {
+        x = a;
+        y = a;
+    }
+    //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << x << endl;
+
+    }
+};
